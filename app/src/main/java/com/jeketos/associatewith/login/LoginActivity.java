2cab9518a8bb2feffac2fdcb1f0d0b3d58ee9f55
Eugene Kotsogub <jeketos@gmail.com>
2016-10-28 14:28:23 +0300

    create login mvp without logic

---
+++ app/src/main/java/com/jeketos/associatewith/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.jeketos.associatewith.login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by eugene.kotsogub on 10/28/16.
+ *
+ */
+
+public class LoginActivity extends AppCompatActivity implements LoginMVP.LoginView {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/jeketos/associatewith/login/LoginMVP.java
@@ -0,0 +1,22 @@
+package com.jeketos.associatewith.login;
+
+/**
+ * Created by eugene.kotsogub on 10/28/16.
+ *
+ */
+
+public class LoginMVP {
+
+    public interface LoginView{
+
+    }
+
+    public interface LoginPresenter{
+
+    }
+
+    public interface LoginModel{
+
+    }
+
+}
