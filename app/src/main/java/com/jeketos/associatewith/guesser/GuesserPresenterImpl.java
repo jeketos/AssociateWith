747efff6d7977cd7a61e9aa6e7c9931dc822e5f9
Eugene Kotsogub <jeketos@gmail.com>
2016-10-28 16:47:36 +0300

    add smart draw logic

--- app/src/main/java/com/jeketos/associatewith/guesser/GuesserPresenterImpl.java
+++ app/src/main/java/com/jeketos/associatewith/guesser/GuesserPresenterImpl.java
@@ -16,21 +16,24 @@ public class GuesserPresenterImpl implements GuesserMVP.GuesserPresenter {
     private GuesserMVP.GuesserView view;
     private GuesserMVP.GuesserModel model;
     private float previousX, previousY;
+    private int previousPointCount;
 
     public GuesserPresenterImpl(GuesserMVP.GuesserView view) {
         this.view = view;
         previousX = 0;
         previousY = 0;
+        previousPointCount = 0;
         model = Injector.provideGuesserModel(this);
         view.init();
     }
 
     @Override
     public void dataReceived(DataSnapshot dataSnapshot) {
+        int ChildrenCount = ((Number) dataSnapshot.getChildrenCount()).intValue();
         if(dataSnapshot.getChildrenCount() == 0){
             view.clearBoard();
         } else {
-            for (int i = 0; i < dataSnapshot.getChildrenCount(); i++) {
+            for (int i = previousPointCount; i < ChildrenCount; i++) {
                 int motionEvent = ((Number) ((HashMap) dataSnapshot.child(Integer.toString(i)).getValue()).get("motionEvent")).intValue();
                 float x = getFloatValue(((HashMap) dataSnapshot.child(Integer.toString(i)).getValue()).get("x"));
                 float y = getFloatValue(((HashMap) dataSnapshot.child(Integer.toString(i)).getValue()).get("y"));
@@ -40,6 +43,7 @@ public class GuesserPresenterImpl implements GuesserMVP.GuesserPresenter {
                 previousY = y;
             }
         }
+        previousPointCount = ChildrenCount;
 
     }
 
